b3e128a0f85b707b4311e3e521d4d06d35eb052d
ssmith2006 <shantel.smith@hotmail.com>
2025-05-28 02:20:33 +0000

    setup dynamodb

---
+++ src/dynamo.js
@@ -0,0 +1,28 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const TABLE = "animals";
+const client = new DynamoDBClient({
+
+region: import.meta.env.VITE_AWS_REGION,
+credentials: {
+  accessKeyID: import.meta.env.VITE_AWS_ACCESS_KEY,
+  secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+},
+
+});
+
+const docClient = DynamoDBDocumentClient(client);
+
+export async function scanAnimals() {
+  const { Items } = await docClient.send;
+  new ScanCommand({
+    TableName: TABLE,
+  });
+
+  return Items || [];
+}
+
+export async function createAnimal(animal) {
+  await docClient.send(new PutCommand({ TableName: TABLE, Item: animal }));
+}
