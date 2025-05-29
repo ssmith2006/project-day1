05507447fec0f1b2c362ddde28e6228893d68fa3
ssmith2006 <shantel.smith@hotmail.com>
2025-05-29 22:23:44 +0000

    Trying to figure out why I can't scan or create an animal with my credentials

--- src/dynamo.js
+++ src/dynamo.js
@@ -1,28 +1,35 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  PutCommand,
+  ScanCommand,
+} from "@aws-sdk/lib-dynamodb";
 
 const TABLE = "animals";
 const client = new DynamoDBClient({
-
-region: import.meta.env.VITE_AWS_REGION,
-credentials: {
-  accessKeyID: import.meta.env.VITE_AWS_ACCESS_KEY,
-  secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
-},
-
+  region: import.meta.env.VITE_AWS_REGION,
+  credentials: {
+    accessKeyID: import.meta.env.VITE_AWS_ACCESS_KEY,
+    secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+  },
 });
 
-const docClient = DynamoDBDocumentClient(client);
+const docClient = DynamoDBDocumentClient.from(client);
 
 export async function scanAnimals() {
-  const { Items } = await docClient.send;
-  new ScanCommand({
-    TableName: TABLE,
-  });
-
+  const { Items} = await docClient.send(
+    new ScanCommand({
+      TableName: TABLE
+    })
+  );
   return Items || [];
 }
 
 export async function createAnimal(animal) {
-  await docClient.send(new PutCommand({ TableName: TABLE, Item: animal }));
+  await docClient.send(
+    new PutCommand({
+      TableName: TABLE,
+      Item: animal,
+    })
+  );
 }
