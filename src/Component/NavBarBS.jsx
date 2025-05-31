c56fb6598e1f4fefb0d2b156bbbc938dec451fc9
ssmith2006 <shantel.smith@hotmail.com>
2025-05-31 02:18:56 +0000

    Added a NavBar

---
+++ src/Component/NavBarBS.jsx
@@ -0,0 +1,20 @@
+import Container from "react-bootstrap/Container";
+
+import Navbar from "react-bootstrap/Navbar";
+
+
+export default function NavBarBS() {
+  return (
+    <Navbar className="bg-body-tertiary">
+    <Container>
+      <Navbar.Brand href="#home">Lovie's Pet Center Rescue Database</Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Signed in as: <a href="#login">Lovie Smith</a>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  );
+}
