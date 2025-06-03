ff8e5ccee5414687ac42c34e88b02d61a815747a
ssmith2006 <shantel.smith@hotmail.com>
2025-06-03 00:35:36 +0000

    created a file

---
+++ src/Component/VolunteerCard.jsx
@@ -0,0 +1,16 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export default function VolunteerOppsCard({
+  title
+  }) {
+  
+
+  return (
+    <div className="container text-center my-5">
+      <h2 className="fs-1 mySubheading">{title}</h2>
+
+      
+    </div>
+  );
+}
