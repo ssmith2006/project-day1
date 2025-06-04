914aa3265b9049282f0ac43cf4d20c7ba4b08b49
ssmith2006 <shantel.smith@hotmail.com>
2025-06-04 02:31:58 +0000

    added objects, useEffect, handle helpers, imported CRUD and components

--- src/Component/VolunteerOppsPage.jsx
+++ src/Component/VolunteerOppsPage.jsx
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
 import VolunteerOppsModal from "./VolunteerModal";
-import { createOpportunity } from "../dynamoVolunteer";
+import {
+  createOpportunity,
+  deleteOpportunity,
+  scanOpportunity,
+  toggleOpportunity,
+} from "../dynamoVolunteer";
+import VolunteerOppsCards from "./VolunteerCards";
+import NavBarBS from "./NavBarBS";
 
 export default function VolunteerOpps() {
   const [form, setForm] = useState({
@@ -9,27 +17,78 @@ export default function VolunteerOpps() {
     date: "",
     location: "",
     isFilled: false,
+    timeSlots: "",
   });
 
-  const [opportunities, showOpportunities] = useState([]);
+  const [opportunities, setOpportunities] = useState([]);
   const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    scanOpportunity().then(setOpportunities);
+  }, []);
 
   async function handleAdd() {
     const item = {
       id: crypto.randomUUID(),
-      title: form.title(),
-      description: form.description(),
-      date: form.date(),
-      location: form.location(),
-      isFilled: form.isFilled(),
+      title: form.title,
+      description: form.description,
+      date: form.date,
+      location: form.location,
+      isFilled: false,
+      timeSlots: form.timeSlots
     };
-    await createOpportunity(item)
+    await createOpportunity(item);
+    setOpportunities((prev) => [...prev, item]);
     setShow(false);
   }
+  function handleChange(e) {
+    const { name, value, type, checked } = e.target;
+    setForm((prev) => {
+      return {
+        ...prev,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
+  }
+
+  async function handleDelete(id) {
+    await deleteOpportunity(id);
+    setOpportunities((prev) =>
+      prev.filter((opportunity) => opportunity.id !== id)
+    );
+  }
+
+  async function handleToggle(opportunity) {
+    await toggleOpportunity(opportunity.id, !opportunity.isFilled);
+    setOpportunities((prev) =>
+      prev.map((o) =>
+        o.id === opportunity.id ? { ...o, isFilled: !o.isFilled } : o
+      )
+    );
+  }
+
   return (
     <>
+    <NavBarBS/>
       <h1>Volunteer Opportunities</h1>
-      <VolunteerOppsModal show={show} />
+      <Button size="lg" variant="primary" onClick={() => setShow(true)}>
+        Add New Opportunity
+      </Button>
+
+      <VolunteerOppsModal
+        show={show}
+        onHide={() => setShow(false)}
+        form={form}
+        onSave={handleAdd}
+        onChange={handleChange}
+      />
+      <VolunteerOppsCards
+        opportunities={opportunities}
+        title="Available Volunteer Opportunities!"
+        noOppsText="No Opportunities Available"
+        onDelete={handleDelete}
+        onOppsToggle={handleToggle}
+      />
     </>
   );
 }
