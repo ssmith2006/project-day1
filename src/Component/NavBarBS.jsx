906a7a6e5eeb91ee4a9e2e0c3b67dfdcce547eae
ssmith2006 <shantel.smith@hotmail.com>
2025-06-04 02:24:26 +0000

    added a Nav link

--- src/Component/NavBarBS.jsx
+++ src/Component/NavBarBS.jsx
@@ -1,21 +1,25 @@
+
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-
+import { Link } from "react-router-dom";
 
 export default function NavBarBS() {
   return (
     <Navbar className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand href="#home">Lovie's Pet Center Rescue Database</Navbar.Brand>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-      
-        <Navbar.Text>
-          Signed in as: <a href="#login">Lovie Smith</a>
-        </Navbar.Text>
-      </Navbar.Collapse>
-
-    </Container>
-  </Navbar>
+      <Container>
+        <Navbar.Brand href="#home">
+          Lovie's Pet Center Rescue Database
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Lovie Smith</a>
+          </Navbar.Text>
+          <div> <Link to={"/admin"}>Volunteer Opportunity Database</Link></div> 
+           
+          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
