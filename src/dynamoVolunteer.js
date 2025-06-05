23d3b249390e4e6ecdfa7b83e5e56c6384c31a2d
ssmith2006 <shantel.smith@hotmail.com>
2025-06-05 23:43:05 +0000

    Modified my DynamoDB operations/functions

--- src/dynamoVolunteer.js
+++ src/dynamoVolunteer.js
@@ -42,3 +42,106 @@ export async function toggleOpportunity(id, isFilled) {
     })
   );
 }
+
+export async function updateOpportunity(id, modalOppsTitle) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #modalOppsTitle = :val",
+      ExpressionAttributeNames: { "#modalOppsTitle": "title" },
+      ExpressionAttributeValues: { ":val": modalOppsTitle},
+    })
+  );
+}
+
+export async function updateOpportunityDescription(id, modalOppsDescription) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #modalOppsDescription = :val",
+      ExpressionAttributeNames: { "#modalOppsDescription": "description" },
+      ExpressionAttributeValues: { ":val": modalOppsDescription },
+    })
+  );
+}
+
+export async function updateOpportunityLocation(id, location) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #location = :val",
+      ExpressionAttributeNames: { "#location": "location" },
+      ExpressionAttributeValues: { ":val": location },
+    })
+  );
+}
+
+export async function updateOpportunity1(id, day1) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #day1 = :val",
+      ExpressionAttributeNames: { "#day1": "day1" },
+      ExpressionAttributeValues: { ":val": day1 },
+    })
+  );
+}
+export async function updateOpportunity2(id, day2) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #day2 = :val",
+      ExpressionAttributeNames: { "#day2": "day2" },
+      ExpressionAttributeValues: { ":val": day2 },
+    })
+  );
+}
+export async function updateOpportunity3(id, day3) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #day3 = :val",
+      ExpressionAttributeNames: { "#day3": "day3" },
+      ExpressionAttributeValues: { ":val": day3 },
+    })
+  );
+}
+export async function updateOpportunity4(id, day4) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #day4 = :val",
+      ExpressionAttributeNames: { "#day4": "day4" },
+      ExpressionAttributeValues: { ":val": day4 },
+    })
+  );
+}
+export async function updateOpportunity5(id, day5) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #day5 = :val",
+      ExpressionAttributeNames: { "#day5": "day5" },
+      ExpressionAttributeValues: { ":val": day5 },
+    })
+  );
+}
+export async function updateOpportunity6(id, day6) {
+  await docClient.send(
+    new UpdateCommand({
+      TableName: TABLE,
+      Key: { id },
+      UpdateExpression: "SET #day6 = :val",
+      ExpressionAttributeNames: { "#day6": "day6" },
+      ExpressionAttributeValues: { ":val": day6 },
+    })
+  );
+}
