5e0010ff56427862c852b47605caf2897c0f7e81
ssmith2006 <shantel.smith@hotmail.com>
2025-06-06 00:34:36 +0000

    Updated my handle Add function with a new state, updated my handleAdd function to update a card, created a function to update the fields on the card, passed down functions to the component

--- src/Component/VolunteerOppsPage.jsx
+++ src/Component/VolunteerOppsPage.jsx
@@ -6,40 +6,92 @@ import {
   deleteOpportunity,
   scanOpportunity,
   toggleOpportunity,
+  updateOpportunity,
+  updateOpportunity1,
+  updateOpportunity2,
+  updateOpportunity3,
+  updateOpportunity4,
+  updateOpportunity5,
+  updateOpportunity6,
+  updateOpportunityDescription,
+  updateOpportunityLocation,
 } from "../dynamoVolunteer";
-import VolunteerOppsCards from "./VolunteerCards";
+import VolunteerCards from "./VolunteerCards";
 import NavBarBS from "./NavBarBS";
 
 export default function VolunteerOpps() {
   const [form, setForm] = useState({
     title: "",
     description: "",
-    days: [],
+    day1: false,
+    day2: false,
+    day3: false,
+    day4: false,
+    day5: false,
+    day6: false,
     location: "",
     isFilled: false,
-    timeSlots: "",
   });
 
   const [opportunities, setOpportunities] = useState([]);
   const [show, setShow] = useState(false);
+  const [editOpportunities, setEditOpportunities] = useState("");
 
   useEffect(() => {
     scanOpportunity().then(setOpportunities);
   }, []);
 
   async function handleAdd() {
-    const item = {
-      id: crypto.randomUUID(),
-      title: form.title,
-      description: form.description,
-      days: form.days,
-      location: form.location,
-      isFilled: false,
-      timeSlots: form.timeSlots
-    };
-    await createOpportunity(item);
-    setOpportunities((prev) => [...prev, item]);
-    setShow(false);
+    if (editOpportunities.id.length > 1) {
+      await handleEditOpps();
+      setForm({
+        title: editOpportunities.title,
+        description: editOpportunities.description,
+        day1: editOpportunities.day1,
+        day2: editOpportunities.day2,
+        day3: editOpportunities.day3,
+        day4: editOpportunities.day4,
+        day5: editOpportunities.day5,
+        day6: editOpportunities.day6,
+        location: editOpportunities.location,
+        isFilled: editOpportunities.isFilled,
+      });
+      setShow(false); //closing the modal//
+      setOpportunities([]) //clear my opps list
+      scanOpportunity().then(setOpportunities); //scanning then storing?????//
+      return;
+    } else {
+      console.log(editOpportunities);
+      const item = {
+        id: crypto.randomUUID(),
+        title: form.title,
+        description: form.description,
+        day1: form.day1,
+        day2: form.day2,
+        day3: form.day3,
+        day4: form.day4,
+        day5: form.day5,
+        day6: form.day6,
+        location: form.location,
+        isFilled: false,
+      };
+
+      await createOpportunity(item);
+      setForm({
+        title: "",
+        description: "",
+        day1: false,
+        day2: false,
+        day3: false,
+        day4: false,
+        day5: false,
+        day6: false,
+        location: "",
+        isFilled: false,
+      });
+      setOpportunities((prev) => [...prev, item]);
+      setShow(false);
+    }
   }
   function handleChange(e) {
     const { name, value, type, checked } = e.target;
@@ -67,6 +119,22 @@ export default function VolunteerOpps() {
     );
   }
 
+  async function handleEditOpps() {
+    await updateOpportunity(editOpportunities.id, form.title);
+    await updateOpportunityDescription(editOpportunities.id , form.description);
+    await updateOpportunityLocation(editOpportunities.id, form.location);
+    await updateOpportunity1(editOpportunities.id, form.day1);
+    await updateOpportunity2(editOpportunities.id, form.day2);
+    await updateOpportunity3(editOpportunities.id, form.day3);
+    await updateOpportunity4(editOpportunities.id, form.day4);
+    await updateOpportunity5(editOpportunities.id, form.day5);
+    await updateOpportunity6(editOpportunities.id, form.day6);
+    setEditOpportunities("");
+  }
+  function handleUpdateOpps(id) {
+    setEditOpportunities(id);
+  }
+
   return (
     <>
       <div className="VolOppsBackground" />
@@ -83,12 +151,14 @@ export default function VolunteerOpps() {
         onSave={handleAdd}
         onChange={handleChange}
       />
-      <VolunteerOppsCards
+      <VolunteerCards
         opportunities={opportunities}
         title="Available Volunteer Opportunities!"
         noOppsText="No Opportunities Available"
         onDelete={handleDelete}
         onOppsToggle={handleToggle}
+        onEditOpps={handleUpdateOpps}
+        onShowModal={() => setShow(true)}
       />
     </>
   );
