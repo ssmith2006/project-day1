4753ae66e3446df32355750ebc2807eda4d657e0
ssmith2006 <shantel.smith@hotmail.com>
2025-06-06 00:36:43 +0000

    deleted an import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home.jsx";
 import VolunteerOppsPage from "./Component/VolunteerOppsPage.jsx";
-import NavBarBS from "./Component/NavBarBS.jsx";
+
 
 export default function App(){
   return (
